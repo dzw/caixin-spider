6f833cfaf98310a687e62aa405a777b6aa730d42
ahxxm <i@ahxxm.com>
2015-08-05 22:14:30 +0800

    add sample article processing code snippet

--- models.py
+++ models.py
@@ -120,13 +120,36 @@ class Article(DeclarativeBase):
     # where:
     #  - `id` is article id(looks unique, but still add 8-digit date for safe..)
     #  - `page=0` to get full text
-    #  - `rand' is optional, ... guess it's just for fun?
-    content = Column(String)
-    content_plain_html = Column(String)
+    #  - `rand' is random.random(), totally optional.. guess it's just for fun?
+    content = Column(String, nullable=True)
+    content_plain_html = Column(String, nullable=True)
 
     # TODO: convenient 1-to-many query for both
     # articles under same issue and publish date
     relate_issue = Column(Integer, ForeignKey('issue.id'))
+
+    def update_content(self, session):
+        """
+
+        :param session: a logged_in requests session
+        :return:
+        """
+        import re
+        # article_url = 'http://weekly.caixin.com/2015-07-10/100828041.html'
+        article_url = self.url
+        article_content = session.get(article_url)
+        java = session.get('http://tag.caixin.com/news/NewsV51.jsp?id=100828060&page=0&rand=0.4237610185518861')
+        article_content_re = re.compile('(?<=resetContentInfo\()[\s\S]*(?=\);)')
+
+        # content
+        article = eval(article_content_re.findall(article_content.text)[0])['content']
+
+        # Clean html to text, leave only <p> tag
+        from lxml.html.clean import Cleaner
+        cleaner = Cleaner(allow_tags=['p'], remove_unknown_tags=False)
+        cleaned_text = cleaner.clean_html(article)
+        cleaned_text = cleaned_text.replace('\u3000', ' ') # content_plain_html
+
 
 
 def test():
