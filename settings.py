b92c8c8a14bf9aca06426bdff9f3e7e6cacc7f32
ahxxm <i@ahxxm.com>
2015-09-23 08:36:49 +0800

    mongodb default settings and a string of 404 sign

--- settings.py
+++ settings.py
@@ -1,26 +1,13 @@
-# SQL settings
+try:
+    from password import USERNAME, PASSWORD
+except:
+    raise ValueError("Specify username/password at `password.py` "
+                     "under the same folder!")
 
-import os
-from password import USERNAME as username, PASSWORD as password
+# Mongodb settings
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.caixin
 
-USERNAME = username
-PASSWORD = password
-
-# for docker-machine user, there will be a environtment variable
-if 'DOCKER_HOST' in os.environ:
-    import re
-    ip_filter = re.compile('(?<=\/\/).*(?=:)')
-    ip = ip_filter.findall(os.environ['DOCKER_HOST'])
-    assert len(ip) == 1 # 
-else:
-    raise ValueError('Fill your database info')
-
-DATABASE = {
-    'drivername': 'postgres',
-    'host': ip[0],
-    'port': 15432,
-    'username': 'dbuser',
-    'password': 'dbpass',
-    'database': 'caixin',
-}
-
+# 404 sign
+sign_of_404 = '<title>404_'
